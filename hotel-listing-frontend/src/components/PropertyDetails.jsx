7bd9626ca0274611849dd160a85def0ca81e582f
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-04 17:53:18 +0530

    Update PropertyDetails.jsx

--- hotel-listing-frontend/src/components/PropertyDetails.jsx
+++ hotel-listing-frontend/src/components/PropertyDetails.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../services/api';
-import '../styles.css';
+import './PropertyDetails.css';
+
 const PropertyDetails = () => {
   const { id } = useParams();
   const [property, setProperty] = useState(null);
@@ -10,7 +11,6 @@ const PropertyDetails = () => {
     const fetchProperty = async () => {
       try {
         const response = await api.get(`/properties/${id}`);
-       
         setProperty(response.data);
       } catch (error) {
         console.error('Error fetching property details:', error);
@@ -21,14 +21,20 @@ const PropertyDetails = () => {
   }, [id]);
 
   if (!property) {
-    return <div>Loading...</div>;
+    return <div className="loading">Loading...</div>;
   }
 
   return (
-    <div className="property-details">
-      <h2>{property.name}</h2>
-      <p>City: {property.city}</p>
-      {/* ...other property details */}
+    <div className="property-details-container">
+      <div className="property-details">
+        <h2>{property.name}</h2>
+        <p>City: {property.city}</p>
+        <p>Bedrooms: {property.bedrooms}</p>
+        <p>Bathrooms: {property.bathrooms}</p>
+        <p>Price: ${property.price}</p>
+        <img src={property.imageUrl} alt={property.name} className="property-image" />
+        {/* ...other property details */}
+      </div>
     </div>
   );
 };
